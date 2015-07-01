23d65d6048a2df80a3ff7ee28132f6ff92755284
duplexa <duplexa@gmail.com>
2015-06-30 21:43:52 -0400

    updated images

--- R/plot_rainbowlines.R
+++ R/plot_rainbowlines.R
@@ -4,13 +4,15 @@
 #' @param mat.list a list of two-column matrices or two-column data frames.
 #' @param samplenames a vector of names that will be used as the legend.text. The length of samplenames must be the same. If not specified, the names of mat.list will be used. Defaults to NULL.
 #' @param color a vector of colors that will be used for the plots. The length of color must be the same as the length of mat.list. If not specified, rainbow colors will be used automatically. Defaults to NULL.
+#' @param lwd line width.
+#' @param lty line type.
 #' @param xlab x-axis label. Defaults to 'x'.
 #' @param ylab y-axis label. Defaults to 'y'.
 #' @param main title. Defaults to ''.
 #' @param cex.legend the cex of the legend. Reduce this if you run out of vertical space for legend. Defaults to 1.
 #' @param template1.arg list of parameters for template1(), such as legend.plt.x.
 #' @param add.func an additional function to add to the plot (eg. abline).
-#' @param ... other graphical parameters including lwd, lty, etc.
+#' @param ... other graphical parameters.
 #' @keywords plot rainbow lines
 #' @export
 #' @return a vector of colors used.
@@ -33,7 +35,7 @@
 #' plot_rainbowlines(mat.list,legend.text,x="values",ylab="Cumulative fraction",main="Gaussian",template1.args=list(legend.plt.x=0.8),add.func=function(){abline(h=0.5,lty=2)})
 #'
 
-plot_rainbowlines<-function(mat.list,samplenames=NULL,color=NULL,xlab="x",ylab="y",main="",cex.legend=1,template1.args=NULL,add.func=NULL,...){
+plot_rainbowlines<-function(mat.list,samplenames=NULL,color=NULL,lty=1,lwd=1,xlab="x",ylab="y",main="",cex.legend=1,template1.args=NULL,add.func=NULL,...){
 
  # set colors
  offset = floor(length(mat.list)/2)  ## this offset ensures that the first and last colors are not too similar.
@@ -55,7 +57,7 @@ plot_rainbowlines<-function(mat.list,samplenames=NULL,color=NULL,xlab="x",ylab="
  func <- function(){
      plot.new()
      plot.window(xlim,ylim)
-     mapply(function(mat,col,...){lines(mat[,1],mat[,2],col=col,...)},mat.list,color,...)
+     mapply(function(mat,col,lwd,lty,...){lines(mat[,1],mat[,2],col=col,lwd=lwd,lty=lty,...)},mat.list,color,lwd=lwd,lty=lty,...)
      box();axis(1);axis(2)
      mtext(xlab,side=1,line=mgp[1])
      mtext(ylab,side=2,line=mgp[1])
@@ -64,7 +66,7 @@ plot_rainbowlines<-function(mat.list,samplenames=NULL,color=NULL,xlab="x",ylab="
  }
 
  legend.func <- function(){
-     legend(0,1,samplenames,col=color,bty="n",cex=cex.legend,xpd=T,...)
+     legend(0,1,samplenames,col=color,bty="n",cex=cex.legend,xpd=T,lwd=lwd,lty=lty,...)
  }
 
  template1.args.all = append(list(func=func,legend.func=legend.func),template1.args)
